e9889a27c21e9d8c79c085a254e4155e025f4f49
cedarchennn <a1249812431@gmail.com>
2019-11-25 14:52:18 +0800

    add index table

--- universitylist/admin.py
+++ universitylist/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question,Choice,Country,Province,Universty,Project
+from .models import Question,Choice,Country,Province,University,Project
 
 admin.site.register(Question)
 admin.site.register(Choice)
 admin.site.register(Country)
 admin.site.register(Province)
-admin.site.register(Universty)
+admin.site.register(University)
 admin.site.register(Project)
