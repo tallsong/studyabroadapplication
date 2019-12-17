a77361a158ad5b2ba098a7e93101db453364a730
cedarchennn <a1249812431@gmail.com>
2019-12-17 10:31:21 +0800

    Fixed a bug where the favicon image could not be loaded

--- studyabroadapplication/urls.py
+++ studyabroadapplication/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path    
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'user/', include('user.urls',namespace='user')),
     path('', include('universitylist.urls',namespace='universitylist')),
-    
+    path('favicon.ico',RedirectView.as_view(url='/static/img/favicon.ico')),   
 ]
